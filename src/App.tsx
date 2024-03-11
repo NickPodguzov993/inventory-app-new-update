a604154d28b21b6056112f7d96470eed7e1495c6
spshedzyal <spshedzyal@it-fintech.com>
2024-03-11 15:05:58 +0300

    Ininial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Wrapper from "./components/Wrapper";
+
+function App() {
+    return (
+        // Wrapper нужно стилизовать в соответствии с требованиями
+        <Wrapper>
+            Hello ;)
+        </Wrapper>
+    )
+}
+
+export default App
